462499542cc4ea1c37ce680b51212bd6f1521d9b
bonaca <abcmemento@gmail.com>
2023-11-16 08:56:06 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang='en'>
+<head>
+<title>ABUENA</title>
+<meta charset="utf-8">
+<meta name='author' content="Edin Sujoldžić">
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<meta name='description' content='lorew description'>
+
+<script src="https://code.jquery.com/jquery-latest.min.js"></script>
+<link href='index.css?<?php echo time(); ?>' rel='stylesheet'>
+</head>
+
+<body>
+<h1>Lorem Ipsum</h1>
+
+
+<script src='index.js?<?php echo time(); ?>'></script>
+
+</body>
+</html>
